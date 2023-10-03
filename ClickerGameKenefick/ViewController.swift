90857d06b0ca0d1ad8a3e3e8d55a5ac1b52bd592
Jake Kenefick <jake.kenefick24@d155.org>
2023-10-03 07:27:31 -0700

    New project

--- ClickerGameKenefick/ViewController.swift
+++ ClickerGameKenefick/ViewController.swift
@@ -11,17 +11,39 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var ScoreOutlet: UILabel!
     
+    var score :Int!
     override func viewDidLoad() {
         super.viewDidLoad()
+        if(score == nil){score = 0}
+        ScoreOutlet.text = "Points:\(score!)"
+        if(score != nil){
+            ScoreOutlet.text = "Points:\(score!)"}
+        self.title = "CLICK THE BUTTON!!!!"
         // Do any additional setup after loading the view.
     }
     
-    var score = 0
+    
+    
+    var multiply = 1
 
+    
+   
     @IBAction func ClickAction(_ sender: Any) {
-        score += 1
-        ScoreOutlet.text = "\(score)"
+        if(score == nil){score = 0}
+        else{
+            score += multiply
+            ScoreOutlet.text = "Points:\(score!)"
+        }
     }
    
+    @IBAction func toStoreAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "toStore", sender: self)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let nvc = segue.destination as! ViewController2
+        nvc.score2 =  score
+        nvc.delegate = self
+    }
 }
 
--- ClickerGameKenefick/ViewController2.swift
+++ ClickerGameKenefick/ViewController2.swift
@@ -8,22 +8,52 @@
 import UIKit
 
 class ViewController2: UIViewController {
-
+    //Creating a variable to give me acces to first view controller
+    var delegate : ViewController!
+    
+    @IBOutlet weak var points2Outlet: UILabel!
+    
+    var score2 : Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        points2Outlet.text = "Points:\(score2!)"
+        self.title = "THE STORE!!!"
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func PointsAction(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "BackToOne", sender: self)
+        func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            
+            let abc = segue.destination as! ViewController
+            abc.score =  score2
+            delegate.score = score2
+            
+        }
     }
-    */
-
+    
+    
+    @IBAction func pointsAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "BackToOne", sender: self)
+        func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            
+            let abc = segue.destination as! ViewController
+            abc.score =  score2
+            delegate.score = score2
+            print("A")
+        }
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
+    }
 }
